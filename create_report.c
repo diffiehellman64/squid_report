53f6722aa9f63573bb85b910838885551c56ecb4
root <root@syk-kom8-px01.RK.local>
2014-01-18 14:09:13 +0400

    function preg_match

--- create_report.c
+++ create_report.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <regex.h>
+#include <sys/types.h>
 
 #define N_FIELDS 10
 
 void parse_log(char *logfile);
+int preg_match(char* src, char* pattern, int field_count);
+//char** preg_match(char* src, char* pattern, int field_count);
 char** split(char *str);
 
 int main(int argc, char* argv[])
 {
 	if (argc > 1)
 		parse_log(argv[1]);
-
+	else
+		printf("Need log file\n");
 	return 0;
 }
 
@@ -21,17 +26,12 @@ char** split(char* str)
 	char **result;
 	char *cell;
 
-	//вообще хорошей практикой является избавление кода от "магических" констант
-	//Например можно было создать глобальную переменную или определить
-	//#define N_FIELDS 10 чтобы код выглядел читабельно
 	result = malloc(N_FIELDS * sizeof(char *));
 	memset(result, 0, N_FIELDS * sizeof(char *));
 
 	cell = strtok(str, " ");
 
-	//надо вставлять проверки на выход из границ массива что бы не было мучительно больно
 	while (cell != NULL && i < N_FIELDS) {
-		//инкремент после присваивания
 		result[i++] = cell;
 		cell = strtok(NULL, " ");
 	}
@@ -42,26 +42,75 @@ char** split(char* str)
 void parse_log (char* logfile)
 {
 	FILE *file;
-	int i, sz;
+	int sz;
 	char *result_string;
-	//parsed это массив указателей на строку
 	char** parsed;
-
+//	char** user;
+	char* pattern;
+	pattern = "([^@]+)@(.*)";
+	int i = 0;
+	//char* site;
 	file = fopen(logfile,"r");
 	result_string = NULL;
-	//когда первый аргумент getline = NULL функция сама выделяет сколько нужно памяти.
 	while(getline(&result_string, &sz, file) > 0) {
+//		printf("str = %d\n", i++);
 		parsed = split (result_string);
-		for (i=0; i < N_FIELDS; ++i) {
-			if (parsed[i])
-				printf("%s ||", parsed[i]);
-		}
-		//\n можно пихать и в саму форматную строку, выглядит понятнее
-		printf("%s", "\n\n");
-		//обязательно освобождай память после использования
+	//	user = parsed[7];
+	//	site = parsed[6];
+//		printf("%s\n", site);
+ //       	user = 
+		preg_match (parsed[7], pattern, 2);
+	//	if (user != 1)
+	//		printf ("%s\n", user[1]);
 		free(result_string);
+	//	free(user);
 		free(parsed);
 		result_string = NULL;
 	}
 	fclose(file);
 }
+//char** preg_match(char* src, char* pattern, int field_count)
+int preg_match(char* src, char* pattern, int field_count)
+{
+   int i = 0;
+   int res;
+   char** f_result;
+   int len;
+   char result[BUFSIZ];
+   char err_buf[BUFSIZ];
+   regex_t preg;
+
+   f_result = malloc(field_count * sizeof(char *));
+   memset(f_result, 0, field_count * sizeof(char *));
+
+   regmatch_t pmatch[10];
+
+   if( (res = regcomp(&preg, pattern, REG_EXTENDED)) != 0)
+   {
+      regerror(res, &preg, err_buf, BUFSIZ);
+      printf("regcomp: %s\n", err_buf);
+      exit(res);
+   }
+
+   res = regexec(&preg, src, 10, pmatch, REG_NOTBOL);
+   if(res == REG_NOMATCH)
+   {
+      printf("NO match\n");
+      return 1;
+   }
+   for (i = 0; i <= field_count; i++)
+   {
+      len = pmatch[i].rm_eo - pmatch[i].rm_so;
+      memcpy(result, src + pmatch[i].rm_so, len);
+      result[len] = 0;
+      f_result[i] = result; 
+//      printf("num %d: '%s'\n", i, result);
+   }
+   printf("fieald1 = %s\n", f_result[0]);
+   printf("fieald2 = %s\n", f_result[1]);
+   printf("fieald3 = %s\n", f_result[2]);
+   regfree(&preg);
+//   return f_result;
+   return 0;
+}
+
