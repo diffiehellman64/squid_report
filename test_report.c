ac1e7c9d97cb3a8456de372050621fbe78170716
root <root@syk-kom8-px01.RK.local>
2014-01-20 14:45:57 +0400

    test report

---
+++ test_report.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <regex.h>
+#include <sys/types.h>
+
+#define N_FIELDS 10
+
+void parse_log(char *logfile);
+int preg_match(char* src, char* pattern, int field_count);
+char** split(char *str);
+char* cut_user(char* str);
+char* cut_site(char* site);
+
+struct squid{
+	char org[10];
+	char user[10];
+	char site[25];
+};
+
+int
+main(int argc, char* argv[])
+{
+	if (argc > 1)
+		parse_log(argv[1]);
+	else
+		printf("Need log file\n");
+	return 0;
+}
+
+char **
+split(char* str)
+{
+	int i = 0;
+	char **result;
+	char *cell;
+
+	result = malloc(N_FIELDS * sizeof(char *));
+	memset(result, 0, N_FIELDS * sizeof(char *));
+
+	cell = strtok(str, " ");
+
+	while (cell != NULL && i < N_FIELDS) {
+		result[i++] = cell;
+		cell = strtok(NULL, " ");
+	}
+
+	return result;
+}
+
+char*
+cut_user(char* str)
+{
+	char* result;
+	int i;
+	//if (user == "- ")
+	//	printf ("ok\n");
+	for (i = 0; i <= strlen(str); ++i)
+	{
+		if (str[i] == '@')
+			str[i] = '\0';
+	}
+	result = malloc(strlen(str) * sizeof(char *));
+	result = str;
+	return result;
+}
+
+char*
+cut_site(char* site)
+{
+	char* result;
+	char* tmp;
+	char* str2;
+	int i;
+	int point_count = 2;
+	for (i = 8; i < strlen(site); ++i)
+	{
+		if (site[i] == '/' || site[i] == ':')
+			site[i] = '\0';
+	}
+	
+	for (i = strlen(site); i >= 0; --i){
+		if (site[i] == '.')
+		{
+			--point_count;
+			tmp = site + i + 1;
+//			printf("%s\n", tmp);
+		}
+		if (site[i] == '/' || point_count == 0)
+		{
+			site = site + i + 1; 
+			break;
+		}
+	}
+//	printf("%s\n", site);
+	result = malloc(strlen(site) * sizeof(char *));
+	result = site;
+	return result;
+}
+
+void
+parse_log(char* logfile)
+{
+	FILE *file;
+	int sz;
+	char *result_string;
+	char** parsed;
+	char* user;
+	int i = 1;
+	char* site;
+	file = fopen(logfile,"r");
+	result_string = NULL;
+	while(getline(&result_string, &sz, file) > 0) {
+//		printf("%d\n", i++);
+		parsed = split (result_string);
+		user = cut_user(parsed[7]);
+		site = cut_site(parsed[6]);
+		printf("%s => %s\n", user, site);
+//		printf("%s\n", site);
+		free(result_string);
+//		free(user);
+		free(parsed);
+		result_string = NULL;
+//		i++;
+	}
+	fclose(file);
+}
